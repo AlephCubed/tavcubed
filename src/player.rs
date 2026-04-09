352106ebbabd979064620ff8dd411b03afcbdc04
AlephCubed <jothebro500@gmail.com>
2026-04-09 11:27:52 -0700

    Track player's current chunk.

--- src/player.rs
+++ src/player.rs
@@ -5,7 +5,9 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup)
+            .init_resource::<PlayerChunk>()
+            .add_systems(Update, player_chunk_changed);
     }
 }
 
@@ -24,4 +26,36 @@ fn setup(mut commands: Commands) {
             ..default()
         },
     ));
+}
+
+#[derive(Resource, Default)]
+pub struct PlayerChunk {
+    pub pos: IVec3,
+}
+
+#[derive(Event, Eq, PartialEq, Clone, Copy)]
+pub struct PlayerChunkChanged {
+    pub old_chunk: IVec3,
+    pub new_chunk: IVec3,
+}
+
+fn player_chunk_changed(
+    mut commands: Commands,
+    mut chunk: ResMut<PlayerChunk>,
+    player: Single<&Transform, (With<Player>, Changed<Transform>)>,
+) {
+    let pos = IVec3 {
+        x: player.translation.x as i32 / 32,
+        y: player.translation.y as i32 / 32,
+        z: player.translation.z as i32 / 32,
+    };
+
+    if chunk.pos != pos {
+        commands.trigger(PlayerChunkChanged {
+            old_chunk: chunk.pos,
+            new_chunk: pos,
+        });
+
+        chunk.pos = pos;
+    }
 }
