4c694260399cd34920cad66f9fa0732c05d46a34
AlephCubed <jothebro500@gmail.com>
2026-04-20 12:22:32 -0700

    Move some of `Chunk` into `Voxel`.

--- src/realm/chunk.rs
+++ src/realm/chunk.rs
@@ -13,12 +13,6 @@ pub struct ChunkPos(pub IVec3);
 
 pub const DIAMETER: usize = 32;
 
-pub const CHUNK_VOXEL_COUNT: usize = DIAMETER * DIAMETER * DIAMETER;
-pub const STRIDE_X: usize = 1;
-pub const STRIDE_Y: usize = DIAMETER;
-pub const STRIDE_Z: usize = DIAMETER * DIAMETER;
-
-pub type VoxelBuffer = [Option<Voxel>; CHUNK_VOXEL_COUNT];
 pub type IntoIter = std::array::IntoIter<Option<Voxel>, CHUNK_VOXEL_COUNT>;
 pub type Iter<'a> = core::slice::Iter<'a, Option<Voxel>>;
 pub type IterMut<'a> = core::slice::IterMut<'a, Option<Voxel>>;
@@ -30,39 +24,6 @@ pub struct Chunk {
 }
 
 impl Chunk {
-    #[inline]
-    fn index_to_pos(index: usize) -> U8Vec3 {
-        debug_assert!(
-            index < CHUNK_VOXEL_COUNT,
-            "Index must be less than {CHUNK_VOXEL_COUNT}, got {index}"
-        );
-        U8Vec3 {
-            x: (index % STRIDE_Y) as u8,
-            y: ((index / STRIDE_Y) % STRIDE_Y) as u8,
-            z: (index / STRIDE_Z) as u8,
-        }
-    }
-
-    #[inline]
-    fn pos_to_index(pos: U8Vec3) -> usize {
-        debug_assert!(
-            pos.x < DIAMETER as u8,
-            "x position must be less than {DIAMETER}, got {}",
-            pos.x
-        );
-        debug_assert!(
-            pos.y < DIAMETER as u8,
-            "y position must be less than {DIAMETER}, got {}",
-            pos.y
-        );
-        debug_assert!(
-            pos.z < DIAMETER as u8,
-            "z position must be less than {DIAMETER}, got {}",
-            pos.z
-        );
-        (pos.z as usize * STRIDE_Z) + (pos.y as usize * STRIDE_Y) + pos.x as usize
-    }
-
     pub fn new(buffer: VoxelBuffer) -> Self {
         Self { buffer }
     }
@@ -207,92 +168,5 @@ impl Default for Chunk {
         Self {
             buffer: [None; CHUNK_VOXEL_COUNT],
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn index_to_pos_x() {
-        for x in 0..32 {
-            assert_eq!(Chunk::index_to_pos(x), U8Vec3::new(x as u8, 0, 0));
-        }
-    }
-
-    #[test]
-    fn pos_to_index_x() {
-        for x in 0..32 {
-            assert_eq!(Chunk::pos_to_index(U8Vec3::new(x as u8, 0, 0)), x);
-        }
-    }
-
-    #[test]
-    fn index_to_pos_y() {
-        for y in 0..32 {
-            assert_eq!(Chunk::index_to_pos(y * 32), U8Vec3::new(0, y as u8, 0));
-        }
-    }
-
-    #[test]
-    fn pos_to_index_y() {
-        for y in 0..32 {
-            assert_eq!(Chunk::pos_to_index(U8Vec3::new(0, y as u8, 0)), y * 32);
-        }
-    }
-
-    #[test]
-    fn index_to_pos_z() {
-        for z in 0..32 {
-            assert_eq!(Chunk::index_to_pos(z * 32 * 32), U8Vec3::new(0, 0, z as u8));
-        }
-    }
-
-    #[test]
-    fn pos_to_index_z() {
-        for z in 0..32 {
-            assert_eq!(Chunk::pos_to_index(U8Vec3::new(0, 0, z as u8)), z * 32 * 32);
-        }
-    }
-
-    #[test]
-    fn index_to_pos_max() {
-        assert_eq!(
-            Chunk::index_to_pos(CHUNK_VOXEL_COUNT - 1),
-            U8Vec3::new(31, 31, 31)
-        );
-    }
-
-    #[test]
-    fn pos_to_index_max() {
-        assert_eq!(
-            Chunk::pos_to_index(U8Vec3::new(31, 31, 31)),
-            CHUNK_VOXEL_COUNT - 1
-        );
-    }
-
-    #[test]
-    #[should_panic(expected = "Index must be less than 32768, got 32768")]
-    fn index_to_pos_invalid() {
-        _ = Chunk::index_to_pos(CHUNK_VOXEL_COUNT)
-    }
-
-    #[test]
-    #[should_panic(expected = "x position must be less than 32, got 32")]
-    fn pos_to_index_invalid_x() {
-        _ = Chunk::pos_to_index(U8Vec3::new(32, 0, 0));
-    }
-
-    #[test]
-    #[should_panic(expected = "y position must be less than 32, got 32")]
-    fn pos_to_index_invalid_y() {
-        _ = Chunk::pos_to_index(U8Vec3::new(16, 32, 16));
-    }
-
-    #[test]
-    #[should_panic(expected = "z position must be less than 32, got 32")]
-    fn pos_to_index_invalid_z() {
-        _ = Chunk::pos_to_index(U8Vec3::new(31, 31, 32));
     }
 }
--- src/realm/chunk/voxel.rs
+++ src/realm/chunk/voxel.rs
@@ -1,4 +1,48 @@
 use crate::realm::block::VoxelId;
+use crate::realm::chunk::{Chunk, DIAMETER};
+use bevy::math::U8Vec3;
+
+pub const CHUNK_VOXEL_COUNT: usize = DIAMETER * DIAMETER * DIAMETER;
+pub const STRIDE_X: usize = 1;
+pub const STRIDE_Y: usize = DIAMETER;
+pub const STRIDE_Z: usize = DIAMETER * DIAMETER;
+
+pub type VoxelBuffer = [Option<Voxel>; CHUNK_VOXEL_COUNT];
+
+impl Chunk {
+    #[inline]
+    pub fn index_to_pos(index: usize) -> U8Vec3 {
+        debug_assert!(
+            index < CHUNK_VOXEL_COUNT,
+            "Index must be less than {CHUNK_VOXEL_COUNT}, got {index}"
+        );
+        U8Vec3 {
+            x: (index % STRIDE_Y) as u8,
+            y: ((index / STRIDE_Y) % STRIDE_Y) as u8,
+            z: (index / STRIDE_Z) as u8,
+        }
+    }
+
+    #[inline]
+    pub fn pos_to_index(pos: U8Vec3) -> usize {
+        debug_assert!(
+            pos.x < DIAMETER as u8,
+            "x position must be less than {DIAMETER}, got {}",
+            pos.x
+        );
+        debug_assert!(
+            pos.y < DIAMETER as u8,
+            "y position must be less than {DIAMETER}, got {}",
+            pos.y
+        );
+        debug_assert!(
+            pos.z < DIAMETER as u8,
+            "z position must be less than {DIAMETER}, got {}",
+            pos.z
+        );
+        (pos.z as usize * STRIDE_Z) + (pos.y as usize * STRIDE_Y) + pos.x as usize
+    }
+}
 
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 
@@ -9,5 +53,92 @@ pub struct Voxel {
 impl Voxel {
     pub fn new(id: VoxelId) -> Self {
         Self { id }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn index_to_pos_x() {
+        for x in 0..32 {
+            assert_eq!(Chunk::index_to_pos(x), U8Vec3::new(x as u8, 0, 0));
+        }
+    }
+
+    #[test]
+    fn pos_to_index_x() {
+        for x in 0..32 {
+            assert_eq!(Chunk::pos_to_index(U8Vec3::new(x as u8, 0, 0)), x);
+        }
+    }
+
+    #[test]
+    fn index_to_pos_y() {
+        for y in 0..32 {
+            assert_eq!(Chunk::index_to_pos(y * 32), U8Vec3::new(0, y as u8, 0));
+        }
+    }
+
+    #[test]
+    fn pos_to_index_y() {
+        for y in 0..32 {
+            assert_eq!(Chunk::pos_to_index(U8Vec3::new(0, y as u8, 0)), y * 32);
+        }
+    }
+
+    #[test]
+    fn index_to_pos_z() {
+        for z in 0..32 {
+            assert_eq!(Chunk::index_to_pos(z * 32 * 32), U8Vec3::new(0, 0, z as u8));
+        }
+    }
+
+    #[test]
+    fn pos_to_index_z() {
+        for z in 0..32 {
+            assert_eq!(Chunk::pos_to_index(U8Vec3::new(0, 0, z as u8)), z * 32 * 32);
+        }
+    }
+
+    #[test]
+    fn index_to_pos_max() {
+        assert_eq!(
+            Chunk::index_to_pos(CHUNK_VOXEL_COUNT - 1),
+            U8Vec3::new(31, 31, 31)
+        );
+    }
+
+    #[test]
+    fn pos_to_index_max() {
+        assert_eq!(
+            Chunk::pos_to_index(U8Vec3::new(31, 31, 31)),
+            CHUNK_VOXEL_COUNT - 1
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Index must be less than 32768, got 32768")]
+    fn index_to_pos_invalid() {
+        _ = Chunk::index_to_pos(CHUNK_VOXEL_COUNT)
+    }
+
+    #[test]
+    #[should_panic(expected = "x position must be less than 32, got 32")]
+    fn pos_to_index_invalid_x() {
+        _ = Chunk::pos_to_index(U8Vec3::new(32, 0, 0));
+    }
+
+    #[test]
+    #[should_panic(expected = "y position must be less than 32, got 32")]
+    fn pos_to_index_invalid_y() {
+        _ = Chunk::pos_to_index(U8Vec3::new(16, 32, 16));
+    }
+
+    #[test]
+    #[should_panic(expected = "z position must be less than 32, got 32")]
+    fn pos_to_index_invalid_z() {
+        _ = Chunk::pos_to_index(U8Vec3::new(31, 31, 32));
     }
 }
