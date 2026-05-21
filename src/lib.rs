7df48922bf642f863f25371f50e6d2280479be7c
AlephCubed <jothebro500@gmail.com>
2026-05-21 09:56:40 -0700

    Change how log levels are handled. This might effect performance, but will be less annoying to deal with.

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,15 @@ pub mod player;
 pub mod realm;
 
 use bevy::camera_controller::free_camera::FreeCameraPlugin;
-use bevy::log::LogPlugin;
 use bevy::pbr::wireframe::WireframePlugin;
 use bevy::prelude::*;
 
 pub fn app() -> App {
     let mut app = App::new();
 
-    app.add_plugins(
-        DefaultPlugins
-            .set(LogPlugin {
-                filter: "tavcubed=debug".to_string(),
-                ..default()
-            })
-            .set(ImagePlugin::default_nearest()),
-    )
-    .add_plugins((FreeCameraPlugin, WireframePlugin::default()))
-    .add_plugins((player::PlayerPlugin, realm::RealmPlugin, debug::DebugPlugin));
+    app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins((FreeCameraPlugin, WireframePlugin::default()))
+        .add_plugins((player::PlayerPlugin, realm::RealmPlugin, debug::DebugPlugin));
 
     app
 }
