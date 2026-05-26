3a529d25f4dedd0f29582122422e93841beb87e5
AlephCubed <jothebro500@gmail.com>
2026-05-26 09:23:04 -0700

    Add argument toggle for VSync.

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,37 @@ pub mod realm;
 use bevy::camera_controller::free_camera::FreeCameraPlugin;
 use bevy::pbr::wireframe::WireframePlugin;
 use bevy::prelude::*;
+use bevy::window::PresentMode;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(long, env, default_value = "true")]
+    vsync: bool,
+}
 
 pub fn app() -> App {
     let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins((FreeCameraPlugin, WireframePlugin::default()))
-        .add_plugins((player::PlayerPlugin, realm::RealmPlugin, debug::DebugPlugin));
+    let args = Args::parse();
+
+    app.add_plugins(
+        DefaultPlugins
+            .set(ImagePlugin::default_nearest())
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    present_mode: if args.vsync {
+                        PresentMode::AutoVsync
+                    } else {
+                        PresentMode::AutoNoVsync
+                    },
+                    ..default()
+                }),
+                ..default()
+            }),
+    )
+    .add_plugins((FreeCameraPlugin, WireframePlugin::default()))
+    .add_plugins((player::PlayerPlugin, realm::RealmPlugin, debug::DebugPlugin));
 
     app
 }
