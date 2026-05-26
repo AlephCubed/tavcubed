1141a7a38599e54d0ae5036425fef30e9b76eca6
AlephCubed <jothebro500@gmail.com>
2026-05-26 09:23:04 -0700

    Disable VSync.

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,24 @@ pub mod realm;
 use bevy::camera_controller::free_camera::FreeCameraPlugin;
 use bevy::pbr::wireframe::WireframePlugin;
 use bevy::prelude::*;
+use bevy::window::PresentMode;
 
 pub fn app() -> App {
     let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins((FreeCameraPlugin, WireframePlugin::default()))
-        .add_plugins((player::PlayerPlugin, realm::RealmPlugin, debug::DebugPlugin));
+    app.add_plugins(
+        DefaultPlugins
+            .set(ImagePlugin::default_nearest())
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    present_mode: PresentMode::AutoNoVsync,
+                    ..default()
+                }),
+                ..default()
+            }),
+    )
+    .add_plugins((FreeCameraPlugin, WireframePlugin::default()))
+    .add_plugins((player::PlayerPlugin, realm::RealmPlugin, debug::DebugPlugin));
 
     app
 }
